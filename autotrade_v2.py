bcbc9032ec7b6f46d530fce95ce7a8ba1bbb572c
Right-Now-Kim <167175239+Right-Now-Kim@users.noreply.github.com>
2024-04-22 16:20:57 +0900

    (test를 위한 시간변경)Update autotrade_v2.py

--- autotrade_v2.py
+++ autotrade_v2.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":
     schedule.every().day.at("08:01").do(make_decision_and_execute)
 
     # Schedule the task to run at 16:01
-    schedule.every().day.at("16:01").do(make_decision_and_execute)
+    schedule.every().day.at("16:22").do(make_decision_and_execute)
 
     while True:
         schedule.run_pending()
